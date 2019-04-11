212109ccca926f7c71e4e794370e25c6f6f3172f
Patrick Conboy <patrick.j.conboy@disney.com>
2019-04-11 15:56:23 -0400

    Initial commit

---
+++ UI/locked-checkbox.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <QCheckBox>
+#include <QPixmap>
+
+class QPaintEvernt;
+
+class LockedCheckBox : public QCheckBox {
+	Q_OBJECT
+
+	QPixmap lockedImage;
+	QPixmap unlockedImage;
+
+public:
+	LockedCheckBox();
+
+protected:
+	void paintEvent(QPaintEvent *event) override;
+};
---
+++ UI/visibility-checkbox.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <QCheckBox>
+#include <QPixmap>
+
+class QPaintEvernt;
+
+class VisibilityCheckBox : public QCheckBox {
+	Q_OBJECT
+
+	QPixmap checkedImage;
+	QPixmap uncheckedImage;
+
+public:
+	VisibilityCheckBox();
+
+protected:
+	void paintEvent(QPaintEvent *event) override;
+};
